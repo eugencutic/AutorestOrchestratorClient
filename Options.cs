98e62700107d07086e06175f0f9dd51c97916fcc
Eugen Cutic <eugen.cutic@uipath.com>
2019-07-18 11:09:49 +0300

    Add project files.

---
+++ Options.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutorestOrchestratorClient
+{
+    class Options
+    {
+        public static readonly string Url = "http://localhost:6234";
+        public static readonly string Username = "admin";
+        public static readonly string Password = "890iop";
+        public static readonly string TenantName = "default";
+        public static readonly string MachineName = "LAPTOP-HJP7A54G";
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Rest;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AutorestOrchestratorClient.Models;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace AutorestOrchestratorClient
+{
+    class Program
+    {
+        private static readonly HttpClient _client = new HttpClient();
+        public static async Task Main(string[] args)
+        {
+            var api = new UiPathWebApi(new BasicAuthenticationCredentials(), _client, false);
+
+            var authResponse = await api.Account.AuthenticateAsync(new LoginModel
+            {
+                UsernameOrEmailAddress = Options.Username,
+                Password = Options.Password,
+                TenancyName = Options.TenantName
+            }).ConfigureAwait(false);
+
+            var token = authResponse.Result as string;
+
+            var api2 = new UiPathWebApi(new TokenCredentials(token), _client, false);
+
+            var robot = new RobotDto("Dummy", "uipath\\eugen.cutic", RobotDtoType.Attended, RobotDtoHostingType.Floating);
+            var robotResponse = await api2.Robots.PostAsync(robot);
+            
+        }  
+    }
+}
